199657fa245fdea77e462ec464796b5e78e4a482
mworzala <mattheworzala@gmail.com>
2023-08-29 20:56:16 -0400

    add failing test

---
+++ demo/src/main/java/net/minestom/demo/commands/TestCommand2.java
@@ -0,0 +1,21 @@
+package net.minestom.demo.commands;
+
+import net.minestom.server.command.builder.Command;
+import net.minestom.server.command.builder.arguments.ArgumentType;
+import org.jetbrains.annotations.NotNull;
+
+public class TestCommand2 extends Command {
+    public TestCommand2() {
+        super("test2");
+
+        var argA = ArgumentType.String("a");
+        var argB = ArgumentType.String("b");
+
+        addSyntax((sender, context) -> {
+            sender.sendMessage("a only");
+        }, argA);
+        addSyntax((sender, context) -> {
+            sender.sendMessage("a and b");
+        }, argA, argB);
+    }
+}
