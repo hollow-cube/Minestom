537403aa8d8f08b9d442e02ce8cd3321ca9a619b
FlxwDNS <friedrichsradik@gmail.com>
2023-10-30 22:32:25 +0100

    Implements Base

--- server/src/main/java/net/bytemc/minestom/server/ByteServer.kt
+++ server/src/main/java/net/bytemc/minestom/server/ByteServer.kt
@@ -1,7 +1,21 @@
 package net.bytemc.minestom.server
 
-class ByteServer {
+import net.bytemc.minestom.server.instance.InstanceHandler
+import net.minestom.server.MinecraftServer
+
+class ByteServer(server: MinecraftServer) {
+    val instanceHandler: InstanceHandler = InstanceHandler()
+
+    init {
+        instance = this
 
 
 
+        server.start("127.0.0.1", 25565)
+        println("[ByteServer] minestom server was started!")
+    }
+
+    companion object {
+        lateinit var instance: ByteServer
+    }
 }
--- server/src/main/java/net/bytemc/minestom/server/ByteServerBootstrap.kt
+++ server/src/main/java/net/bytemc/minestom/server/ByteServerBootstrap.kt
@@ -1,5 +1,12 @@
 package net.bytemc.minestom.server
 
+import net.minestom.server.MinecraftServer
+
 fun main() {
-    ByteServer()
+    println("[ByteServer] initializing minestom server...")
+
+    val server = MinecraftServer.init()
+
+    println("[ByteServer] initializing byteServer...")
+    ByteServer(server)
 }
