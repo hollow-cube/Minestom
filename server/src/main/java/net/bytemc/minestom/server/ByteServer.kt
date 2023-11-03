f65f11416760dc1344faa2907c4b73f7c4e176d6
HttxMarco <mircolindenau04@gmail.com>
2023-11-03 09:52:24 +0100

    Add default block handler

--- server/src/main/java/net/bytemc/minestom/server/ByteServer.kt
+++ server/src/main/java/net/bytemc/minestom/server/ByteServer.kt
@@ -1,13 +1,19 @@
 package net.bytemc.minestom.server
 
+import net.bytemc.minestom.server.handler.BlockHandlers
 import net.bytemc.minestom.server.instance.InstanceHandler
 import net.minestom.server.MinecraftServer
+import net.minestom.server.utils.NamespaceID
+
 
 class ByteServer(server: MinecraftServer) {
     val instanceHandler = InstanceHandler()
 
     init {
         instance = this
+
+        MinecraftServer.getBlockManager().registerHandler(NamespaceID.from("minecraft:skull")) { BlockHandlers.SKULL_HANDLER }
+        MinecraftServer.getBlockManager().registerHandler(NamespaceID.from("minecraft:banner")) { BlockHandlers.BANNER_HELPER }
 
         server.start("127.0.0.1", 25565)
         println("[ByteServer] minestom server was started!")
