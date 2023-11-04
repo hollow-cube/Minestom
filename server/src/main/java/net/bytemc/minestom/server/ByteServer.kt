ba60ae8dcb77b4156a61d2a2c8dacb1d6b8d8670
HttpMarco <mirco.lindenau@gmx.de>
2023-11-04 12:34:32 +0100

    Implement interact item

--- server/src/main/java/net/bytemc/minestom/server/ByteServer.kt
+++ server/src/main/java/net/bytemc/minestom/server/ByteServer.kt
@@ -1,10 +1,14 @@
 package net.bytemc.minestom.server
 
-import net.bytemc.minestom.server.clickable.ClickableEntity
+import net.bytemc.minestom.server.clickable.InteractItem
 import net.bytemc.minestom.server.handler.BlockHandlers
 import net.bytemc.minestom.server.instance.InstanceHandler
 import net.minestom.server.MinecraftServer
+import net.minestom.server.event.player.PlayerBlockBreakEvent
+import net.minestom.server.item.ItemStack
+import net.minestom.server.item.Material
 import net.minestom.server.utils.NamespaceID
+import java.util.function.Consumer
 
 
 class ByteServer(server: MinecraftServer) {
@@ -19,8 +23,16 @@ class ByteServer(server: MinecraftServer) {
         server.start("127.0.0.1", 25565)
         println("[ByteServer] minestom server was started!")
 
-        /*
-        fakePlayerHandler.register("", PlayerSkin.fromUsername("FlxwDNS")!!, instanceHandler.getSpawnInstance(), Pos(0.0, 44.0, 0.0))*/
+        MinecraftServer.getGlobalEventHandler().addListener(PlayerBlockBreakEvent::class.java) { event ->
+            var item = InteractItem(ItemStack.of(Material.STICK)) {
+                run {
+                    it.sendMessage("§aYou clicked the item!")
+
+                }
+            }
+            item.equip(event.player, 0)
+        }
+
     }
 
     companion object {
---
+++ server/src/main/java/net/bytemc/minestom/server/clickable/InteractItem.java
@@ -0,0 +1,45 @@
+package net.bytemc.minestom.server.clickable;
+
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.entity.Player;
+import net.minestom.server.event.player.PlayerUseItemEvent;
+import net.minestom.server.item.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class InteractItem {
+
+    private static final List<InteractItem> ITEM_LIST = new ArrayList<>();
+
+    static {
+        MinecraftServer.getGlobalEventHandler().addListener(PlayerUseItemEvent.class, event -> {
+            //TODO FIX IF BlockPlacementListener.java line 58
+            for (InteractItem item : ITEM_LIST) {
+                if (event.getPlayer().getItemInMainHand().equals(item.item)) {
+                    item.interactionPlayer.accept(event.getPlayer());
+                }
+                break;
+            }
+        });
+    }
+
+    private ItemStack item;
+    private Consumer<Player> interactionPlayer;
+
+    public InteractItem(ItemStack item, Consumer<Player> interactionPlayer) {
+        this.item = item;
+        this.interactionPlayer = interactionPlayer;
+
+        ITEM_LIST.add(this);
+    }
+
+    public void equip(Player player, int slot) {
+        player.getInventory().setItemStack(slot, this.item);
+    }
+
+    public void remove() {
+        ITEM_LIST.remove(this);
+    }
+}
