0ae72d2e0324a28205fa2ab5912c742203a75bf5
FlxwDNS <friedrichsradik@gmail.com>
2023-11-05 22:06:54 +0100

    Begin HeadDisplay implementation

---
+++ server/src/main/java/net/bytemc/minestom/server/display/head/HeadDisplay.kt
@@ -0,0 +1,14 @@
+package net.bytemc.minestom.server.display.head
+
+import net.minestom.server.coordinate.Point
+
+class HeadDisplay(val value: String, val pos: Point, size: HeadSize = HeadSize.MID) {
+
+    fun spawn() {
+        
+    }
+
+    fun destroy() {
+        TODO("not implemented yet.")
+    }
+}
