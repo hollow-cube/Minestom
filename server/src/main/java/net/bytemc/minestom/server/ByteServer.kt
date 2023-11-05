4bae436fa10257b6db219a68e90ec58e561080a0
FlxwDNS <friedrichsradik@gmail.com>
2023-11-05 22:06:11 +0100

    Implement --allowTesting arg

--- server/src/main/java/net/bytemc/minestom/server/ByteServer.kt
+++ server/src/main/java/net/bytemc/minestom/server/ByteServer.kt
@@ -2,8 +2,10 @@ package net.bytemc.minestom.server
 
 import net.bytemc.minestom.server.clickable.InteractItem
 import net.bytemc.minestom.server.handler.BlockHandlers
+import net.bytemc.minestom.server.hologram.Hologram
 import net.bytemc.minestom.server.instance.InstanceHandler
 import net.minestom.server.MinecraftServer
+import net.minestom.server.coordinate.Pos
 import net.minestom.server.event.player.PlayerBlockBreakEvent
 import net.minestom.server.item.ItemStack
 import net.minestom.server.item.Material
@@ -11,7 +13,7 @@ import net.minestom.server.utils.NamespaceID
 import java.util.function.Consumer
 
 
-class ByteServer(server: MinecraftServer) {
+class ByteServer(server: MinecraftServer, args: Array<String>) {
     val instanceHandler = InstanceHandler()
 
     init {
@@ -23,15 +25,31 @@ class ByteServer(server: MinecraftServer) {
         server.start("127.0.0.1", 25565)
         println("[ByteServer] minestom server was started!")
 
+        // Testing
+        if(args.contains("--allowTesting")) {
+            println("[ByteServer] allowTesting...")
+            testInstance()
+        }
+    }
+
+    fun testInstance() {
+        // BlockBreak event
         MinecraftServer.getGlobalEventHandler().addListener(PlayerBlockBreakEvent::class.java) { event ->
-            var item = InteractItem(ItemStack.of(Material.STICK)) {
+            val instance = instanceHandler.getSpawnInstance()
+
+            // InteractItem
+            val item = InteractItem(ItemStack.of(Material.STICK)) {
                 run {
                     it.sendMessage("§aYou clicked the item!")
 
                 }
             }
             item.equip(event.player, 0)
+
+            // Hologram
+            Hologram(instance, Pos(1.0, 5.0, 1.0), listOf("Test", "Test2")).spawn()
         }
+
 
     }
 
