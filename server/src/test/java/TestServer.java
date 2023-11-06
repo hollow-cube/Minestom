ba36f24cde14b2a6d5689997b5fc0e8e2866c4e8
FlxwDNS <friedrichsradik@gmail.com>
2023-11-06 18:14:36 +0100

    added more letters

--- server/src/test/java/TestServer.java
+++ server/src/test/java/TestServer.java
@@ -1,5 +1,6 @@
 import net.bytemc.minestom.server.ByteServer;
 import net.bytemc.minestom.server.display.head.HeadDisplay;
+import net.bytemc.minestom.server.display.head.misc.HeadSize;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.utils.Direction;
 
@@ -11,7 +12,20 @@ public final class TestServer {
         // HeadDisplay
         var head = new HeadDisplay("Love you Marco Polo", instance, new Pos(1, 2, 1), settings -> {
             settings.withDirection(Direction.NORTH);
+            settings.withHeadSize(HeadSize.BIG);
         });
         head.spawn();
+
+        var head2 = new HeadDisplay("Welcome to ByteMC.DE", instance, new Pos(1, 2.25, 1), settings -> {
+            settings.withDirection(Direction.NORTH);
+            settings.withHeadSize(HeadSize.MID);
+        });
+        head2.spawn();
+
+        var head3 = new HeadDisplay("0 Player online", instance, new Pos(1, 1.25, 1), settings -> {
+            settings.withDirection(Direction.NORTH);
+            settings.withHeadSize(HeadSize.MID);
+        });
+        head3.spawn();
     }
 }
