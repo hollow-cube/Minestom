0f606916c9309944dd9a2f32a93bbddd9f4a6f7c
FlxwDNS <friedrichsradik@gmail.com>
2023-11-06 17:56:27 +0100

    updated testing

--- server/src/test/java/TestServer.java
+++ server/src/test/java/TestServer.java
@@ -1,6 +1,7 @@
 import net.bytemc.minestom.server.ByteServer;
 import net.bytemc.minestom.server.display.head.HeadDisplay;
 import net.minestom.server.coordinate.Pos;
+import net.minestom.server.utils.Direction;
 
 public final class TestServer {
 
@@ -9,7 +10,7 @@ public final class TestServer {
 
         // HeadDisplay
         var head = new HeadDisplay("Test Marco Polo", instance, new Pos(1, 2, 1), settings -> {
-
+            settings.withDirection(Direction.NORTH);
         });
         head.spawn();
     }
