3f620cb1d34dc2e8f0cf1dee442e763380342056
FlxwDNS <friedrichsradik@gmail.com>
2023-11-07 22:30:13 +0100

    test implemented methods

--- server/src/test/java/TestServer.java
+++ server/src/test/java/TestServer.java
@@ -3,13 +3,24 @@ import net.bytemc.minestom.server.clickable.ClickableEntity;
 import net.bytemc.minestom.server.display.head.HeadDisplay;
 import net.bytemc.minestom.server.display.head.misc.HeadSize;
 import net.bytemc.minestom.server.hologram.Hologram;
+import net.bytemc.minestom.server.schematics.CuboId;
+import net.bytemc.minestom.server.schematics.Rotation;
+import net.bytemc.minestom.server.schematics.Schematic;
+import net.bytemc.minestom.server.schematics.manager.SchematicBuilder;
+import net.bytemc.minestom.server.schematics.manager.SchematicReader;
+import net.bytemc.minestom.server.schematics.manager.SchematicWriter;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.EntityType;
 import net.minestom.server.entity.GameMode;
+import net.minestom.server.entity.PlayerSkin;
+import net.minestom.server.entity.fakeplayer.FakePlayer;
 import net.minestom.server.event.player.PlayerBlockBreakEvent;
 import net.minestom.server.event.player.PlayerSpawnEvent;
 import net.minestom.server.utils.Direction;
+
+import java.io.IOException;
+import java.nio.file.Path;
 
 public final class TestServer {
 
@@ -38,16 +49,49 @@ public final class TestServer {
         head3.spawn();
 
         var entity = new ClickableEntity(EntityType.ALLAY, player -> {
-            player.sendMessage("Click!");
+            player.sendMessage("Click! ClickableEntity");
         });
         entity.modify(it -> {
             it.setNoGravity(true);
         });
         entity.spawn(new Pos(1, 10, 1), instance);
 
+        FakePlayer.spawnPlayer(PlayerSkin.fromUsername("HttpMarco"), instance, new Pos(2, 2, 2), fakePlayer -> {
+            fakePlayer.getInteractController().subscribe(player -> {
+                player.sendMessage("Click! FakePlayer");
+            });
+        });
+
         MinecraftServer.getGlobalEventHandler().addListener(PlayerBlockBreakEvent.class, event -> {
             var holo = new Hologram(event.getBlockPosition().add(0, 3, 0), instance, "Test", "Test2");
             holo.spawn();
+
+            // schematic test
+            var cuboId = new CuboId(new Pos(0, 0, 0), event.getBlockPosition(), instance);
+            var builder = SchematicBuilder.builder(cuboId);
+            var schematic = builder.toSchematic();
+
+            schematic.build(Rotation.NONE, block -> {
+                return block;
+            }).apply(instance, 0, 15, 0, () -> {
+
+            });
+
+            /*
+            try {
+                SchematicWriter.write(schematic, Path.of("schematics/test.dat"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }*/
+
+            /*try {
+                var schematic2 = SchematicReader.read(Path.of("schematics/test.dat"));
+                schematic2.build(Rotation.CLOCKWISE_90, block -> {
+                    return block;
+                });
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }*/
         });
 
         MinecraftServer.getGlobalEventHandler().addListener(PlayerSpawnEvent.class, event -> {
