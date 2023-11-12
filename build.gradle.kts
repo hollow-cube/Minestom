711f7a1952ed8f4d67437ce630c4d52e9b0efc88
FlxwDNS <friedrichsradik@gmail.com>
2023-11-12 20:46:17 +0100

    updated version

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ allprojects {
     apply(plugin = "maven-publish")
 
     group = "net.bytemc"
-    version = "1.0.3-SNAPSHOT"
+    version = "1.0.4-SNAPSHOT"
 
     repositories {
         mavenCentral()
