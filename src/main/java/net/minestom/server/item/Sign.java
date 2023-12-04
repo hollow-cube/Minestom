80cb5ae1a444ebba3ad0fa55a8390b3764a43243
I-am-Tobias <68862867+I-am-Tobias@users.noreply.github.com>
2023-12-04 18:06:49 +0100

    fixed Sign with static text not updating after breaking

--- src/main/java/net/minestom/server/item/Sign.java
+++ src/main/java/net/minestom/server/item/Sign.java
@@ -14,6 +14,7 @@ import net.minestom.server.event.player.PlayerBlockBreakEvent;
 import net.minestom.server.event.trait.InstanceEvent;
 import net.minestom.server.instance.Instance;
 import net.minestom.server.instance.block.Block;
+import net.minestom.server.network.packet.server.play.BlockChangePacket;
 import net.minestom.server.network.packet.server.play.BlockEntityDataPacket;
 import net.minestom.server.utils.Direction;
 import org.jetbrains.annotations.NotNull;
@@ -68,6 +69,7 @@ public class Sign {
 
     public void updateTextForPlayer(@NotNull Player player) {
         if (staticText) {
+            player.sendPacket(new BlockChangePacket(position, instance.getBlock(position)));
             return;
         }
         NBTCompound nbt;
