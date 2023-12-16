0ac8c33d46e88fddfe3fe8374a8e98ece7fd6477
mworzala <mattheworzala@gmail.com>
2023-12-16 09:07:39 +0200

    chore: use common function to generate invalid state errors

--- src/main/java/net/minestom/server/network/packet/server/play/DeclareRecipesPacket.java
+++ src/main/java/net/minestom/server/network/packet/server/play/DeclareRecipesPacket.java
@@ -5,6 +5,7 @@ import net.minestom.server.network.ConnectionState;
 import net.minestom.server.network.NetworkBuffer;
 import net.minestom.server.network.packet.server.ServerPacket;
 import net.minestom.server.network.packet.server.ServerPacketIdentifier;
+import net.minestom.server.utils.PacketUtils;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -47,7 +48,7 @@ public record DeclareRecipesPacket(@NotNull List<DeclaredRecipe> recipes) implem
     public int getId(@NotNull ConnectionState state) {
         return switch (state) {
             case PLAY -> ServerPacketIdentifier.DECLARE_RECIPES;
-            default -> throw new IllegalArgumentException();
+            default -> PacketUtils.invalidPacketState(getClass(), state, ConnectionState.PLAY);
         };
     }
 
