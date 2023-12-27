b73aafd28319b096d3dac8b5e48d21ccb88eb1fd
Gatt <1311244+RealGatt@users.noreply.github.com>
2023-12-27 13:08:27 +1100

    Import PacketUtils

--- src/main/java/net/minestom/server/network/packet/server/play/DeclareRecipesPacket.java
+++ src/main/java/net/minestom/server/network/packet/server/play/DeclareRecipesPacket.java
@@ -6,6 +6,7 @@ import net.minestom.server.network.NetworkBuffer;
 import net.minestom.server.network.packet.server.ServerPacket;
 import net.minestom.server.network.packet.server.ServerPacketIdentifier;
 import net.minestom.server.recipe.RecipeCategory;
+import net.minestom.server.utils.PacketUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
